8e4b94c60847e059a0dd315e243b3fbebea1c788
Chris Garcia <chrisgarcia001@gmail.com>
2014-05-19 21:49:40 -0400

    Updating code

--- script.R
+++ script.R
@@ -1,7 +1,6 @@
 filename <- "repdata_data_StormData.csv.bz2"
 #all.data <- subset(read.csv("repdata_data_StormData.csv.bz2", header=TRUE), !is.na(EVTYPE))
-#all.data <- subset(read.table("repdata_data_StormData.csv.bz2", quote="\"s", sep=",", header=TRUE), !is.na(EVTYPE))
-raw.data <- all.data[1:15000,]
+raw.data <- all.data[1:50000,]
 
 # STEP 1: Extract data by event type by health and economic harm.
 injuries <- c()
@@ -12,27 +11,31 @@ cropdam <- c()
 totalcost <- c()
 events <- unique(raw.data$EVTYPE)
 
+print(as.character(events))
 for(i in 1:length(events)) {
 	curr <- subset(raw.data, as.character(EVTYPE) == as.character(events[i]))
-	injuries[i] <- sum(curr$INJURIES, na.rm = TRUE)
-	fatalities[i] <- sum(curr$FATALITIES, na.rm = TRUE)
+	injuries[i] <- sum(as.numeric(curr$INJURIES), na.rm = TRUE)
+	fatalities[i] <- sum(as.numeric(curr$FATALITIES), na.rm = TRUE)
 	totalhealth[i] <- injuries[i] + fatalities[i]
-	propdam <- sum(curr$PROPDMG, na.rm = TRUE)
-	cropdam <- sum(curr$CROPDMG, na.rm = TRUE)
-	totalcost <- propdam[i] + cropdam[i]
+	propdam[i] <- sum(as.numeric(curr$PROPDMG), na.rm = TRUE)
+	cropdam[i] <- sum(as.numeric(curr$CROPDMG), na.rm = TRUE)
+	totalcost[i] <- propdam[i] + cropdam[i]
 }
+
 
 # STEP 2: Prepare appropriate data frames
 n <- 10
 #print(events[1:n])
-print(as.character(events))
-health <- data.frame(cbind(et = as.character(events), inj=injuries, fat=fatalities, tot=as.numeric(totalhealth)))
-print(head(health))
-health <- health[with(health, order(-tot)), ]
-print(health)
+#print(as.character(events))
+health <- cbind(et = as.character(events), inj=injuries, fat=fatalities, totalhealth)
+#health$total
+#health <- health[with(health, order(-totalhealth)), ]
+
+#print(health)
+
 #economic <- data.frame(cbind(evt=events, propdam, cropdam, cost = totalcost))
 #economic <- economic[order(-totalcost)][1:n, ]
 
 # Step 3a: Prepare Health Chart
-with(health, plot(et, tot, col="green"))
+plot(factor(health[,1]), as.numeric(health[,4]), type="h", col="blue")
 
