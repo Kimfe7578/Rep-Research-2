a8c44e08a331b9c000e610a9990457d264da49e1
Chris Garcia <chrisgarcia001@gmail.com>
2014-05-20 19:33:45 -0400

    Plot update

--- script.R
+++ script.R
@@ -1,6 +1,6 @@
 filename <- "repdata_data_StormData.csv.bz2"
 #all.data <- subset(read.csv("repdata_data_StormData.csv.bz2", header=TRUE), !is.na(EVTYPE))
-raw.data <- all.data[1:200000,]
+raw.data <- all.data[1:5000,]
 
 
 # STEP 1: Extract data by event type by health and economic harm.
@@ -31,7 +31,7 @@ par(mfrow = c(2, 1))
 # STEP 2: Prepare and plot health data
 health <- data.frame(evt=as.character(events), inj=injuries, fat=fatalities, th=totalhealth)
 health <- health[with(health, order(-th)), ][1:min(n, nrow(health)), ]
-title1.str <- paste("Overall Health Incidents by Event Type (Top", min(n, nrow(health)), "Highest Total)")
+title1.str <- paste("Fig. 1: Overall Health Incidents (Top", min(n, nrow(health)), "Highest Totals)")
 with(health,{
 	plot(1:length(evt), th, type="b", pch=17, lwd=3, col="blue", 
 	     axes = FALSE, xlab = "Event Type", ylab = "Total Incidents on Record", 
@@ -46,10 +46,10 @@ with(health,{
 # Step 3: Prepare and plot economic data
 economic <- data.frame(evt=as.character(events), prop=propdam, crop=cropdam, tc=totalcost)
 economic <- economic[with(economic, order(-tc)), ][1:min(n, nrow(economic)), ]
-title2.str <- paste("Overall Economic Impact by Event Type (Top", min(n, nrow(health)), "Highest Total Losses)")
+title2.str <- paste("Fig 2: Overall Economic Impact (Largest", min(n, nrow(health)), "Total Losses)")
 with(economic,{
 	plot(1:length(evt), tc, type="b", pch=17, lwd=3, col="blue", 
-	     axes = FALSE, xlab = "Event Type", ylab = "Total Losses in USD($)", 
+	     axes = FALSE, xlab = "Event Type", ylab = "Losses in USD($)", 
 		 main=title2.str)
 	lines(1:length(evt), prop, type="b", pch=17, lwd=3, col="red")
 	lines(1:length(evt), crop, type="b", pch=17, lwd=3, col="green")
